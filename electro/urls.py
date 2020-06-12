1bedb6b675fcf1d95cc8b924db33050d4666ffc3
Chacoul-Diass <57750236+Chacoul-Diass@users.noreply.github.com>
2020-06-12 15:10:44 +0000

    vue home

--- electro/urls.py
+++ electro/urls.py
@@ -5,4 +5,7 @@ from electro import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('erreur', views.erreur, name='erreur'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
 ]
--- electro/views.py
+++ electro/views.py
@@ -2,4 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index
+def index (request):
+    return render(request, 'pages/home/index.html')
+
+def erreur (request):
+    return render(request, 'pages/home/404.html')
+
+def about (request):
+    return render(request, 'pages/home/about.html')
+
+def contact (request):
+    return render(request, 'pages/home/contact-v1.html')
