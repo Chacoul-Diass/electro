bfcb4f24fae0d11551d3ffb6385a4beee18ac4f0
Romaric Ano <jeannoelromaricano@gmail.com>
2020-06-12 21:30:19 +0000

    Make static for blog Configuration for blog

--- projet/settings.py
+++ projet/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'electro.apps.ElectroConfig',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
--- projet/urls.py
+++ projet/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('electro.urls')),
+    path('', include('blog.urls')),
 ]
 
 if settings.DEBUG:
