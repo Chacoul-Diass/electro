5267d1b5599eaad62f0da746a091434b1227629e
Abrahamkone <62753365+Abrahamkone@users.noreply.github.com>
2020-06-12 15:13:45 +0000

    ib

--- electro/urls.py
+++ electro/urls.py
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-rom django.urls import path, include
+from django.urls import path, include
 
 from electro import views
 
@@ -7,11 +6,3 @@ from electro import views
 urlpatterns = [
     path('', views.index, name='home'),
 ]
-=======
-from django.contrib import admin
-from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
->>>>>>> 7ee67a873ef8ac268ab1591a9ebe07c633fefe34
