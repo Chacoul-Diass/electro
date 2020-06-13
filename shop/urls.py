4834543b994feaa57f1342d5cdb6c76ecfe00438
Daouda <ouattaradaouda4613@gmail.com>
2020-06-13 03:19:45 +0000

    urls and views in shop

--- shop/urls.py
+++ shop/urls.py
@@ -2,6 +2,16 @@ from django.urls import path
 
 from . import views
 
+app_name = "shop"
+
 urlpatterns = [
+	path('cart', views.cart, name="cart"),
+	path('chechout', views.chechout, name="chechout"),
+	path('my-account', views.account, name="my-account"),
+	path('product-categorie', views.product_categorie, name="product-categorie"),
+	path('shop-list', views.shop_list, name="shop-list"),
+	path('shop', views.shop, name="shop"),
+	path('single-product', views.single_product, name="single-product"),
+	path('wishlist', views.wishlist, name="wishlist"),
     
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def cart(request):
+	return render(request, "shop/cart.html")
+
+def checkout(request):
+	return render(request, "shop/checkout.html")
+
+def account(request):
+	return render(request, "shop/my-account.html")
+
+def product_categorie(request):
+	return render(request, "shop/product-categorie.html")
+
+def shop-list(request):
+	return render(request, "shop/shop-list.html")
+
+def shop(request):
+	return render(request, "shop/shop.html")
+
+def single_product(request):
+	return render(request, "shop/single-product.html")
+
+def wishlist(request):
+	return render(request, "shop/single-product.html")
