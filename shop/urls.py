bbd87067ebc3600eaa925f311cf19a0421ccec85
225junior <225juniorbrindou@gmail.com>
2020-06-13 10:46:47 +0000

    Creation de routes: shop:home shop:single-product shop:product-categorie-

--- shop/urls.py
+++ shop/urls.py
@@ -5,13 +5,26 @@ from . import views
 app_name = "shop"
 
 urlpatterns = [
-	path('cart', views.cart, name="cart"),
-	path('checkout', views.checkout, name="checkout"),
-	path('my-account', views.account, name="my-account"),
-	path('product-categorie', views.product_categorie, name="product-categorie"),
-	path('shop-list', views.shop_list, name="shop-list"),
-	path('shop', views.shop, name="shop"),
+	path('', views.shop, name="home"),
+
 	path('single-product', views.single_product, name="single-product"),
+
+	path('product-categorie', views.product_categorie, name="product-categorie"),
+
+
+
+	
+
+	path('cart', views.cart, name="cart"),
+
+	path('checkout', views.checkout, name="checkout"),
+
+	path('my-account', views.account, name="my-account"),
+
+
+	path('shop-list', views.shop_list, name="shop-list"),
+
+
 	path('wishlist', views.wishlist, name="wishlist"),
     
 ]
--- shop/views.py
+++ shop/views.py
@@ -2,6 +2,21 @@ from django.shortcuts import render
 
 # Create your views here.
 
+# lien → {% url 'shop:home' %}
+def shop(request):
+	return render(request, "shop/shop.html")
+
+# lien → {% url 'shop:single-product' %}
+def single_product(request):
+	return render(request, "shop/single-product-fullwidth.html")
+
+# lien → {% url 'shop:product-categorie' %}
+def product_categorie(request):
+	return render(request, "shop/product-categorie.html")
+
+
+
+
 def cart(request):
 	return render(request, "shop/cart.html")
 
@@ -11,17 +26,10 @@ def checkout(request):
 def account(request):
 	return render(request, "shop/my-account.html")
 
-def product_categorie(request):
-	return render(request, "shop/product-categorie.html")
-
 def shop_list(request):
 	return render(request, "shop/shop-list.html")
 
-def shop(request):
-	return render(request, "shop/shop.html")
-
-def single_product(request):
-	return render(request, "shop/single-product.html")
 
 def wishlist(request):
 	return render(request, "shop/single-product.html")
+
