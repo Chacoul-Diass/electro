7037a99574eb12782926b149e8bcf1e67b1412c0
225junior <225juniorbrindou@gmail.com>
2020-06-16 15:17:32 +0000

    migration pour dynamiser base.html

--- electro/admin.py
+++ electro/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from electro import models 
 
 # Register your models here.
+
+
+class InfoAdmin(admin.ModelAdmin):
+	list_display = (
+		'telephone',
+		'telephone2',
+		'email',
+		'address',
+		'date_cre',
+		'date_mod',
+	)
+
+	fieldsets = [
+		('Info', {
+			'fields':[
+				'telephone',
+				'telephone2',
+				'email',
+				'address',
+					]
+		}),
+	]
+
+
+def _register(model, admin_class):
+    admin.site.register(model, admin_class)
+
+_register(models.Info, InfoAdmin)
--- electro/models.py
+++ electro/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Info(models.Model):
+	telephone = models.CharField(default='99999999', max_length=8)
+	telephone2 = models.CharField(default='99999999', max_length=8)
+
+	email = models.CharField(default='electro@email.com', max_length=50)
+
+	address = models.CharField(max_length=255, default='17 Princess Road, London, Greater London NW1 8JR, UK')
+
+	date_cre = models.DateTimeField(auto_now_add=True)
+	date_mod = models.DateTimeField(auto_now=True)
+
+	class Meta:
+		verbose_name = "Info"
+		verbose_name_plural = "Infos"
+
+	def __str__(self):
+		return self.address
