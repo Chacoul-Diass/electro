3415b99623fd5b01c62a3d2779c51e47958e2402
225junior <225juniorbrindou@gmail.com>
2020-06-18 15:53:25 +0000

    modification des liens

--- shop/urls.py
+++ shop/urls.py
@@ -9,22 +9,4 @@ urlpatterns = [
 
 	path('single-product/<int:id>/', views.single_product, name="single-product"),
 
-	path('product-categorie', views.product_categorie, name="product-categorie"),
-
-
-
-
-
-	path('cart', views.cart, name="cart"),
-
-	path('checkout', views.checkout, name="checkout"),
-
-	path('my-account', views.account, name="my-account"),
-
-
-	path('shop-list', views.shop_list, name="shop-list"),
-
-
-	path('wishlist', views.wishlist, name="wishlist"),
-    
 ]
--- shop/views.py
+++ shop/views.py
@@ -7,12 +7,6 @@ from django.core.paginator import Paginator
 # lien → {% url 'shop:home' %}
 def shop(request):
 
-	productList = models.Product.objects.all()
-	paginator = Paginator(productList, 10) # affiche 10 produits per page
-
-	page = request.GET.get('page')
-	produits = paginator.get_page(page)
-
 	data = {
 	# tous les departements
 	'departements':models.Departement.objects.all(),
@@ -29,7 +23,7 @@ def shop(request):
 	'info':Info.objects.first(),
 	}
 
-	return render(request, "shop/shop.html",data,produits)
+	return render(request, "shop/shop.html",data)
 
 
 
@@ -43,52 +37,3 @@ def single_product(request, id):
 	}
 
 	return render(request, "shop/single-product-fullwidth.html",data)
-
-# lien → {% url 'shop:product-categorie' %}
-def product_categorie(request):
-	data = {
-        'info':Info.objects.first(),
-    }
-	return render(request, "shop/product-categorie.html",data)
-
-
-
-
-def cart(request):
-	data = {
-        'info':Info.objects.first(),
-    }
-	return render(request, "shop/cart.html",data)
-
-
-
-def checkout(request):
-	data = {
-        'info':Info.objects.first(),
-    }
-	return render(request, "shop/checkout.html",data)
-
-
-
-def account(request):
-	data = {
-        'info':Info.objects.first(),
-    }
-	return render(request, "shop/my-account.html",data)
-
-
-
-def shop_list(request):
-	data = {
-        'info':Info.objects.first(),
-    }
-	return render(request, "shop/shop-list.html",data)
-
-
-
-def wishlist(request):
-	data = {
-        'info':Info.objects.first(),
-    }
-	return render(request, "shop/single-product.html",data)
-
